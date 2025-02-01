253b1df66bea00c14029b5e8cec0275d053b9d67
rajan chaurasiya <rajan2662000@gmail.com>
2025-02-01 12:10:39 +0530

    removed some unused import packages

--- src/test/java/pageObject/homePage.java
+++ src/test/java/pageObject/homePage.java
@@ -3,10 +3,7 @@ package pageObject;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
 
 /**
  * homePage class that represents the home page of the application.
